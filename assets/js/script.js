20b1869817ae74462135be46b5701ff6ed6ef9a3
matthewmahler <matthewjamesmahler@gmail.com>
2018-08-22 08:11:04 -0400

    progress and volume bars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,54 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
 
+function formatTime(duration) {
+  var time = Math.round(duration);
+  var minutes = Math.floor(time / 60);
+  var seconds = time - (minutes * 60);
 
+  var extraZero = (seconds < 10) ? "0" : "";
+
+  return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+  $(".progressTime.current").text(formatTime(audio.currentTime))
+  $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime))
+
+  var progress = ((audio.currentTime / audio.duration) * 100);
+  $(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio) {
+  var volume = (audio.volume * 100);
+  $(".volumeBar .progress").css("width", volume + "%");
+}
 
 class Audio {
   constructor() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
-    this.setTrack = function (src) {
-      this.audio.src = src;
+
+    this.audio.addEventListener('canplay', function () {
+      var duration = formatTime(this.duration);
+      $(".progressTime.remaining").text(duration);
+
+    });
+
+    this.audio.addEventListener('timeupdate', function () {
+      if (this.duration) {
+        updateTimeProgressBar(this);
+      }
+    })
+
+    this.audio.addEventListener('volumechange', function () {
+      updateVolumeProgressBar(this);
+    })
+
+    this.setTrack = function (track) {
+      this.currentlyPlaying = track;
+      this.audio.src = track.path;
     };
     this.play = function () {
       this.audio.play();
@@ -16,5 +56,8 @@ class Audio {
     this.pause = function () {
       this.audio.pause();
     };
+    this.setTime = function (seconds) {
+      this.audio.currentTime = seconds;
+    }
   }
 }
--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -11,32 +11,115 @@ $jsonArray = json_encode($resultArray);
 
 <script>
 
+  //song started == true
+  //song played false
+  //song played 25%, 50%, 75%, songEnded
+
 $(document).ready(function(){
   currentPlaylist = <?php echo $jsonArray; ?>;
   audioElement = new Audio();
   setTrack(currentPlaylist[0], currentPlaylist, false)
+  updateVolumeProgressBar(audioElement.audio);
+  $("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e){
+    e.preventDefault();
+  })
+//playback
+
+  $(".playbackBar .progressBar").mousedown(function(){
+    mouseDown = true;
+  })
+
+   $(".playbackBar .progressBar").mousemove(function(e){
+     if (mouseDown){
+      timeFromOffset(e, this);
+     }
+  })
+  $(".playbackBar .progressBar").mouseup(function(e){
+       timeFromOffset(e, this);
+
+  })
+
+  $(document).mouseup(function(){
+    mouseDown = false
+  })
+
+
+//volume
+
+   $(".volumeBar .progressBar").mousedown(function(){
+    mouseDown = true;
+  })
+
+   $(".volumeBar .progressBar").mousemove(function(e){
+     if (mouseDown){
+      var percentage = e.offsetX / $(this).width();
+
+      if (percentage >= 0 && percentage <= 1){
+          audioElement.audio.volume = percentage;
+      }
+
+     }
+  })
+  $(".volumeBar .progressBar").mouseup(function(e){
+    var percentage = e.offsetX / $(this).width();
+
+if (percentage >= 0 && percentage <= 1){
+    audioElement.audio.volume = percentage;
+}
+  })
+
+
 });
 
+function timeFromOffset(mouse, progressBar){
+  var percentage = mouse.offsetX / $(progressBar).width() * 100;
+  var seconds = audioElement.audio.duration * (percentage / 100);
+audioElement.setTime(seconds);
+}
+
 function setTrack(trackId, newPlaylist, play){
+  $.post("./includes/handlers/ajax/getSongJSON.php", {songId: trackId}, function(trackData){
+    var track = JSON.parse(trackData);
+    $("span.trackNamePlayer").text(track.title);
 
+    $.post("./includes/handlers/ajax/getArtistJSON.php", {artistId: track.artist}, function(artistData){
+      var artist = JSON.parse(artistData);
+      $("span.artistNamePlayer").text(artist.name);
+    })
 
+    $.post("./includes/handlers/ajax/getAlbumJSON.php", {albumId: track.album}, function(albumData){
+      var album = JSON.parse(albumData);
+      $(".albumLink img").attr("src", album.artworkPath);
+    })
 
-  audioElement.setTrack("");
+    audioElement.setTrack(track);
+    playSong();
+  })
+
 
   if (play){
     audioElement.play();
+    $(".controlButton.play").hide();
+    $(".controlButton.pause").show();
   }
 }
+
 function playSong(){
+
+  if(audioElement.audio.currentTime == 0){
+    $.post("./includes/handlers/ajax/updatePlays.php", {songId: audioElement.currentlyPlaying.id});
+  }
     $(".controlButton.play").hide();
     $(".controlButton.pause").show();
     audioElement.play();
   }
+
   function pauseSong(){
     $(".controlButton.play").show();
     $(".controlButton.pause").hide();
     audioElement.pause();
   }
+
 </script>
 
 <div id="nowPlayingBarContainer">
@@ -44,11 +127,11 @@ function playSong(){
         <div id="nowPlayingLeft">
           <div class="content">
             <span class="albumLink">
-              <img src="./assets/images/icons/Musical Notes_104px.png" class="albumArt">
+              <img src="" class="albumArt">
             </span>
             <div class="trackInfo">
-              <span class="trackName">Track Name</span>
-              <span class="artistName">Artist Name</span>
+              <span class="trackNamePlayer"></span>
+              <span class="artistNamePlayer"></span>
             </div>
 
           </div>
